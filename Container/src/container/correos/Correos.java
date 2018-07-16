9bb06cc452413cc1db5f04ebbd81e09579e66626
ulacit <ulacit@docente.ulacit.ac.cr>
2018-07-16 13:56:02 -0600

    trabajo en clase
    trabajo en clase

---
+++ Container/src/container/correos/Correos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package container.correos;
+
+/**
+ *
+ * @author ulacit
+ */
+public class Correos {
+    private String correo ;
+    public Correos(String correos) {
+        correo = correos;
+}
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(int correo) {
+        this.setCorreo(correo);
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+}
+    
+
